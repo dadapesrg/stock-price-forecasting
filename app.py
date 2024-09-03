60a3474347e25202c3bbfe60661291d8a02feb43
update-branch <josephodada@gmail.com>
2024-09-03 09:41:19 +0100

    Update on the app

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ app = Flask(__name__)
 import pickle
 with open('results/lstm_stock_model.pkl', 'rb') as f:
     model = pickle.load(f)
-# Load the pre-trained LSTM model
-#model = load_model('results/lstm_stock_model.h5')
 
 # Initialize a MinMaxScaler (assuming the same scaler was used during training)
 scaler = MinMaxScaler(feature_range=(0, 1))
@@ -24,19 +22,17 @@ def predict():
     data = request.json
     input_data = np.array(data['input'])
     features = np.array(input_data).reshape(-1, 1)
-   
-    print(features)
+       
     # Transform the data to fit shape of the LSTM input)
     scaled_features = scaler.fit_transform(features)
-    print("Before tconversion:", scaled_features)
+    
     # Repeat the features until it reaches the necessary length
     features_repeated = np.tile(scaled_features, (60, 1))  # Repeats to get a shape of (60, 5)
     scaled_features = features_repeated.reshape(1, 60, 5)  # Reshpae according to model
-        
-    print("After transform: \n", scaled_features)
-    # Predict
+      
+    # Make predictions for 'Close' price in the 4th column
     prediction = model.predict(scaled_features)
-    prediction = scaler.inverse_transform(np.zeros((1, 5)) + prediction)[0, 3]  # Assuming 'Close' price is the 4th column
+    prediction = scaler.inverse_transform(np.zeros((1, 5)) + prediction)[0, 3]  # 'Close' price is the 4th column
 
     return jsonify({'prediction': prediction.tolist()})
 
